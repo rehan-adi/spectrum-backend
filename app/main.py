43bd04505bcbb97d8d9dc651414454da4ea69b12
Rehan <rehanalire52@gmail.com>
2025-02-27 22:10:54 +0530

    feat: Improve logging and configuration in main application setup

--- app/main.py
+++ app/main.py
@@ -1,30 +1,37 @@
 from fastapi import FastAPI
+from app.core.logger import logger
+from app.core.config import settings
+from app.routes.auth import auth_router
 from contextlib import asynccontextmanager
 from app.routes.health import health_router
 from app.db.db import db_connect, db_disconnect
 from fastapi.middleware.cors import CORSMiddleware
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup
     await db_connect()
-    print("✅ Database connected!")
-    
+    logger.info("✅ Database connect")
+
     yield
 
     # Shutdown
     await db_disconnect()
-    print("❌ Database disconnected!")
+    logger.info("❌ Database disconnected!")
 
-app = FastAPI(lifespan=lifespan);
 
+app = FastAPI(lifespan=lifespan)
+
+# Middlewares
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=settings.ALLOWED_METHODS,
+    allow_headers=settings.ALLOWED_HEADERS,
 )
 
-# Health Check Route
-app.include_router(health_router, tags=["Health Check"], prefix="/api/v1")
+# Routes
+app.include_router(health_router, tags=["Health Route"], prefix="/api/v1/health")
+app.include_router(auth_router, tags=["Authentication Routes"], prefix="/api/v1/auth")

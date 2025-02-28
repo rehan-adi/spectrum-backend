226089f8854e7ee68b53d0e79308e9e2a0d21897
Rehan <rehanalire52@gmail.com>
2025-02-28 20:43:56 +0530

    feat: implement signin endpoint with token generation and password verification

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,8 +1,9 @@
 from app.db.db import prisma
 from app.core.logger import logger
-from app.schemas.auth import Signup
-from app.utils.password import hash_password
-from fastapi import APIRouter, HTTPException, status
+from app.schemas.auth import Signup, Signin
+from app.utils.token import create_access_token
+from app.utils.password import hash_password, verify_password
+from fastapi import APIRouter, HTTPException, status, Response
 
 auth_router = APIRouter()
 
@@ -36,3 +37,38 @@ async def signup(data: Signup):
     except Exception:
         logger.error("DB Error while creating user", exc_info=True)
         raise HTTPException(status_code=500, detail="DB Error while creating user")
+
+
+@auth_router.post("/signin", status_code=status.HTTP_200_OK)
+async def signin(data: Signin, response: Response):
+    email = data.email
+    password = data.password
+
+    try:
+        user = await prisma.user.find_first(where={"email": email})
+    except Exception:
+        logger.error("Failed to get user details from db", exc_info=True)
+        raise HTTPException(status_code=500, detail="fuck")
+
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    valid_password = verify_password(password, user.password)
+
+    if not valid_password:
+        raise HTTPException(
+            status_code=401, detail="Incorrect password. Please try again."
+        )
+
+    token = create_access_token(user_id=user.id)
+
+    response.set_cookie(
+        key="token",
+        value=f"Bearer {token}",
+        httponly=False,
+        max_age=2 * 24 * 60 * 60,
+        samesite="lax",
+        secure=True,
+    )
+
+    return {"status": "success", "token": token, "message": "Login successful"}

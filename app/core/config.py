61cbb7426a1e6e1b5a63395adc343702b6e275b9
Rehan <rehanalire52@gmail.com>
2025-02-28 20:43:02 +0530

    feat: add SECRET_KEY and ACCESS_TOKEN_EXPIRE_MINUTES to configuration settings

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,8 @@ load_dotenv()
 
 class Settings:
     DATABASE_URL = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.getenv("SECRET_KEY") or "secret"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 2 # 2 days
     ALLOWED_ORIGINS: List[str] = ["*"]
     ALLOWED_METHODS: List[str] = ["*"]
     ALLOWED_HEADERS: List[str] = ["*"]

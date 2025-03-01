4e3b2644584ab793366506d844ab4038121811ad
Rehan <rehanalire52@gmail.com>
2025-03-01 21:48:58 +0530

    feat: add endpoint to upgrade user role to artist

--- app/routes/profile.py
+++ app/routes/profile.py
@@ -1,15 +1,16 @@
 from app.db.db import prisma
+from prisma.enums import Role
 from app.core.logger import logger
 from app.utils.token import get_user_id
 from app.schemas.profile import UserProfile
-from app.schemas.response import ApiResponse, ResponseStatus
 from fastapi import APIRouter, Depends, HTTPException, status
+from app.schemas.response import ApiResponse, ResponseStatus, MessageResponse
 
 profile_router = APIRouter()
 
 
 @profile_router.get(
-    "/", status_code=status.HTTP_200_OK, response_model=ApiResponse[UserProfile]
+    "/me", status_code=status.HTTP_200_OK, response_model=ApiResponse[UserProfile]
 )
 async def get_profile(user_id: str = Depends(get_user_id)):
     try:
@@ -37,6 +38,32 @@ async def get_profile(user_id: str = Depends(get_user_id)):
 
     except Exception:
         logger.error("DB error while getting user profile", exc_info=True)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal server error",
+        )
+
+
+@profile_router.post(
+    "/upgrade-to-artist", status_code=status.HTTP_200_OK, response_model=MessageResponse
+)
+async def upgrade_to_artist(user_id: str = Depends(get_user_id)):
+    try:
+        user = await prisma.user.find_unique(where={"id": user_id})
+
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+            )
+        await prisma.user.update(where={"id": user_id}, data={"role": Role.Artist})
+
+        return MessageResponse(
+            status=ResponseStatus.SUCCESS,
+            message="User upgraded to artist successfully",
+        )
+
+    except Exception:
+        logger.error("DB error while updating user profile", exc_info=True)
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Internal server error",
